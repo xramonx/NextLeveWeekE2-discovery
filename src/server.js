a487b5fc0764be9d48821a3afb6e7d5f32aefb0d
Ramon Menezes <xramonx@gmail.com>
2020-08-25 00:24:23 -0300

    Dia 4 completo - inicio backend
    criado servidor com express , automatizado criação de html com nunjunks, alterado dados e disciplinas de efomra dinamica (preparando para implementação de banco de dados).
    
    utilização de nodemon
    
    inserido

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-const proffys = [
+//DATA
+let proffys = [
     {
         name: "Diego Fernandes",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
@@ -9,7 +10,7 @@ const proffys = [
         weekday: [0],
         time_from: [720],
         time_to: [1220]
-    },
+    },  
     {
         name: "Daniele Evangelista",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
@@ -23,35 +24,85 @@ const proffys = [
     }
 ]
 
+const subjects = [
+"Artes",
+"Biologia",
+"Ciências",
+"Educação física",
+"Física",
+"Geografia",
+"História",
+"Matemática",
+"Português",
+"Química",
+]
+
+
+const weekdays = [
+"Domingo",
+"Segunda-feira",
+"Terça-feira",
+"Quarta-feira",
+"Quinta-feira",
+"Sexta-feira",
+"Sábado",
+]
 
 const title = "hello world from Nunjucks"
 
+//Functionallities
+
+function getSubject(subjectNumber){
+    const position = +subjectNumber-1
+    return subjects[position]
+}
 function pageLanding(req, res){
     return res.render("index.html") //sem nunjucks -> res.sendFile(__dirname + "/views/index.html")
 }
 
 function pageStudy(req,res){
-    return res.render("study.html", {proffy: proffys[0]})
+    console.log(req.query) //verificar tudo o que está após a barra - req.query
+    const filters = req.query
+    return res.render("study.html", {proffys, filters, subjects, weekdays})
 }
 
 function pageGiveClasses(req,res){
-    return res.render("give-classes.html")
+    const data = req.query
+    //adicionar dados a lista de proffys
+
+    console.log(data)
+    //console.log(proffys)
+    
+    if(Object.keys(data).length != 0){//se dado não estiver vazio
+        //console.log("/n/n/n/entrei aqui")
+        data.subject = getSubject(data.subject)
+        proffys.push(data)
+        //console.log(proffys)
+        
+    }    
+
+
+    return res.render("give-classes.html", {subjects, weekdays})
+    //return res.redirect("/study")
 }
 
-
+//Server creation
 const express = require('express')
 const server = express()
-const nunjucks = require('nunjucks')
 
-//configurar nunjucks
+
+//configurar nunjucks 
+const nunjucks = require('nunjucks')
 nunjucks.configure('src/views',{
     express: server,
     noCache: true,
 })
 
+//início e configuração do servidor
 server
     //configurar arquivsos estáticos (css, scripts, imagens)
     .use(express.static("public"))//configuraçao
+    
     //rotas da aplicação
     .get("/", pageLanding)// .get("/give-classes", (req, res) => {return res.sendFile(__dirname + "/views/give-classes.html")})
     .get("/study", pageStudy)
